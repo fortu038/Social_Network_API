bbad379b1040e5b900d7061728cf12b75c10e369
fortu038 <fortu038@umn.edu>
2023-06-15 15:33:40 -0500

    Reformatted userControllers and updated README.

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,121 +7,133 @@ const friendCount = async () => {
     .then((numFriends) => numFriends);
 }
 
-module.exports = {
-  getUsers(req, res) {
-    User.find()
-      .then(async (users) => {
-        const userObj = {
-          users,
-        };
-        return res.json(userObj);
-      })
-      .catch((err) => {
-        console.log(err);
-        return res.status(500).json(err);
-      });
-  },
+const getAllUsers = async (req, res) => {
+  try {
+    const getAllQuery = await User.find({});
+    res.status(200).json({ result: "success", payload: getAllQuery });
+  }
+  catch(err) {
+    res.status(404).json({ message: "Unable to find any users. Please check your connection or try again later." });
+  }
+};
 
-  getSingleUser(req, res) {
-    // console.log(">>>getSingleUser");
-    // res.sendStatus(200);
-    User.findOne({ _id: req.params.userId })
-    .select('-__v')
-    .then(async (user) =>
-      !user
-        ? res.status(404).json({ message: 'No user with that ID' })
-        : res.json({
-            user,
-          })
-    )
-    .catch((err) => {
-      console.log(err);
-      return res.status(500).json(err);
-    });
-  },
+const getUserById = async (req, res) => {
+  try {
+    const getByIdQuery = await User.findById(req.params.userId)
+    res.status(200).json({ result: "success", payload: getByIdQuery });
+  }
+  catch(err) {
+    res.status(400).json({ message: "No user found with that ID!" });
+  }
+};
 
-  createUser(req, res) {
-    // console.log(">>>createUser");
-    // res.sendStatus(200);
-    User.create(req.body)
-      .then((user) => res.json(user))
-      .catch((err) => res.status(500).json(err));
-  },
+const createUser = async (req, res) => {
+  try {
+    const createQuery = await User.create(req.body);
+    res.status(201).json({result: "success", payload: createQuery});
+  }
+  catch(err) {
+    const usernameCheck = await User.findOne({username: req.body.username});
+    if(usernameCheck != null) {
+      res.status(400).json({message: "This username is already in use."});
+      return;
+    }
 
-  updateUser(req, res) {
-    // console.log(">>>updateUser");
-    // res.sendStatus(200);
-    User.findOneAndUpdate(
-      { _id: req.params.userId },
+    const emailCheck = await User.findOne({email: req.body.email});
+    if(emailCheck != null) {
+      res.status(400).json({message: "This email is already in use."});
+      return;
+    }
+    res.status(404).json({ message: "An error occured while trying create a user. Please check your connection or try again later." });
+  }
+};
+
+const updateUserById = async (req, res) => {
+  try {
+    const updateByIdQuery = await User.findOneAndUpdate(
+      {_id: req.params.userId},
       { $set: req.body },
       { runValidators: true, new: true }
-    )
-      .then((updatedUser) =>
-        !updatedUser
-          ? res.status(404).json({ message: 'No user with this ID!' })
-          : res.json(updatedUser)
-      )
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  },
+    );
+    res.status(200).json({ result: "success", payload: updateByIdQuery });
+  }
+  catch(err) {
+    const userCheck = await User.findById(req.params.userId);
+    if(userCheck == null) {
+      res.status(400).json({ message: "No user found with that ID, cannot update!" });
+      return;
+    }
+    res.status(404).json({ message: "An error occured while trying to update this user. Please check your connection or try again later."});
+  }
+};
 
-  deleteUser(req, res) {
-    // console.log(">>>deleteUser");
-    // res.sendStatus(200);
-    User.findOneAndRemove({ _id: req.params.userId })
-      .then((user) =>
-        !user
-          ? res.status(404).json({ message: 'No such user exists' })
-          : Thought.findOneAndUpdate(
-              { username: user.username },
-              { $pull: { users: req.params.username } },
-              { new: true }
-            )
-      )
-      .then((thought) =>
-        !thought
-          ? res.status(404).json({
-              message: 'User deleted, but no thoughts found',
-            })
-          : res.json({ message: 'User successfully deleted' })
-      )
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  },
+const deleteUserById = async (req,res) => {
+  try {
+    const delByIdQuery = await User.findOneAndDelete({ _id: req.params.userId });
 
-  addFriend(req, res) {
-    // console.log(">>>addFriend");
-    // res.sendStatus(200);
-    User.findOneAndUpdate(
+    res.status(200).json({ result: "success", payload: delByIdQuery });
+  }
+  catch(err) {
+    res.status(400).json({ message: "No user found with that ID!" });
+  }
+};
+
+const addFriendById = async (req, res) =>  {
+  try {
+    const addFriendByIdQuery = await User.findOneAndUpdate(
       {_id: req.params.userId},
       {$addToSet: { friends: req.params.friendId }},
       {runValidators: true, new: true}
-    )
-      .then((user) =>
-        !user
-          ? res.status(404).json({ message: 'No user with this id!' })
-          : res.json(user)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
+    );
+    res.status(200).json({ result: "success", payload: addFriendByIdQuery });
+  }
+  catch(err) {
+    const userCheck = findById(req.params.userId);
+    if(userCheck == null) {
+      res.status(400).json({ message: "No user with that ID found!"});
+      return;
+    }
 
-  deleteFriend(req, res) {
-    // console.log(">>>deleteFriend");
-    // res.sendStatus(200);
-    User.findOneAndUpdate(
+    const friendCheck = findById(req.params.friendId);
+    if(friendCheck == null) {
+      res.status(400).json({ message: "No friend with that ID found!"});
+      return;
+    }
+
+    res.status(404).json({ message: "An error occured while trying to add a friend. Please check your connection or try again later." });
+  }
+};
+
+const deleteFriendById = async (req, res) => {
+  try {
+    const deleteFriendByIdQuery = await User.findOneAndUpdate(
       {_id: req.params.userId},
       {$pull: {friends: req.params.friendId}},
       {runValidators: true, new: true}
-    )
-      .then((user) =>
-        !user
-          ? res.status(404).json({ message: 'No user with this id!' })
-          : res.json(user)
-      )
-      .catch((err) => res.status(500).json(err));
+    );
+    res.status(200).json({ result: "success", payload: deleteFriendByIdQuery });
   }
+  catch(err) {
+    const userCheck = findById(req.params.userId);
+    if(userCheck == null) {
+      res.status(400).json({ message: "No user with that ID found!"});
+      return;
+    }
+
+    if(!userCheck.friends.includes(req.params.friendId)) {
+      res.status(400).json({ message: "No friend with that ID found in friends list!"});
+      return;
+    }
+    res.status(404).json({ message: "An error occured while trying to remove a friend. Please check your connection or try again later." });
+  }
+};
+
+module.exports = { 
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUserById,
+  deleteUserById,
+  addFriendById,
+  deleteFriendById
 };
