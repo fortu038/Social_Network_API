00ce8165edbfb64bfd9a63044d954894512e6a52
fortu038 <fortu038@umn.edu>
2023-06-20 15:39:22 -0500

    Updated error messaging in thoughtController.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -8,7 +8,7 @@ const getAllThoughts = async (req, res) => {
     res.status(200).json({ result: "success", payload: getAllQuery });
   }
   catch(err) {
-    res.status(404).json({ message: "Unable to find any thoughts. Please check your connection or try again later." });
+    res.status(404).json({ message: "A connection issue has occured. Please check your connection or try again later." });
   }
 };
 
@@ -16,10 +16,14 @@ const getThoughtById = async (req, res) => {
   try {
     const getByIdQuery = await Thought.findById(req.params.thoughtId);
 
+    if(getByIdQuery === null) {
+      res.status(400).json({ message: "No thought with that ID found!" })
+    }
+
     res.status(200).json({ result: "success", payload: getByIdQuery });
   }
   catch(err) {
-    res.status(400).json({ message: "No thought found with that ID!" });
+    res.status(404).json({ message: "A connection issue has occured. Please check your connection or try again later." });
   }
 };
 
@@ -36,6 +40,19 @@ const createThought = async (req, res) => {
     res.status(201).json({result: "success", payload: createQuery});
   }
   catch(err) {
+    const txt = req.body.thoughtText;
+    const usr = req.body.username;
+
+    if(txt === undefined) {
+      res.status(400).json({ message: "No thought text provided." });
+      return;
+    }
+
+    if(usr === undefined) {
+      res.status(400).json({ message: "No username associated with this thought." });
+      return;
+    }
+
     res.status(404).json({ message: "An error occured while trying create a thought. Please check your connection or try again later." });
   }
 };
@@ -48,6 +65,11 @@ const updateThoughtById = async (req, res) => {
       { runValidators: true, new: true }
     );
 
+    if(updateByIdQuery === null) {
+      res.status(400).json({ message: "No thought with that ID found so no updates could be made" });
+      return;
+    }
+
     res.status(200).json({ result: "success", payload: updateByIdQuery });
   }
   catch(err) {
@@ -57,20 +79,22 @@ const updateThoughtById = async (req, res) => {
 
 const deleteThoughtById = async (req, res) => {
   try {
-    const delByIdQuery = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+    const thought_id = req.params.thoughtId
+
+    const delByIdQuery = await Thought.findOneAndDelete({ _id: thought_id });
     
-    if(delByIdQuery == null) {
+    if(delByIdQuery === null) {
       res.status(400).json({ message: "No thought found with that ID!" });
       return;
     }
 
     const ownerOfThoughtQuery = await User.findOneAndUpdate(
       {username: delByIdQuery.username},
-      {$pull: {thoughts: req.params.thoughtId}},
+      {$pull: {thoughts: thought_id}},
       {runValidators: true, new: true}
     );
 
-    if(ownerOfThoughtQuery == null) {
+    if(ownerOfThoughtQuery === null) {
       res.status(404).json({ message: "Thought deleted but no associated user found."});
       return;
     }
@@ -90,18 +114,52 @@ const addReaction = async (req, res) => {
       { runValidators: true, new: true}
     );
 
+    if(addReactionQuery === null) {
+      res.status(400).json({ message: "No thought with that ID found so no reactions could be added." });
+      return;
+    }
+
     res.status(200).json({ result: "success", payload: addReactionQuery })
   }
   catch(err) {
+    const body = req.body;
+
+    if(body.username === undefined) {
+      res.status(400).json({ message: "No username assoicated with this reaction so it cannot be posted." });
+      return;
+    }
+
+    if(body.reactionBody === undefined) {
+      res.status(400).json({ message: "This reaction has no body text so it can't be posted." });
+      return;
+    }
+
     res.status(404).json({ message: "An error occured while trying to add a reaction. Please check your connection or try again later." });
   }
 };
 
 const deleteReactionById = async (req, res) => {
   try {
+    const thought_id = req.params.thoughtId;
+    const reaction_id = req.params.reactionId;
+
+    const thoughtCheck = await Thought.findOne({ _id: thought_id });
+
+    if(thoughtCheck === null) {
+      res.status(400).json({ message: "No thought with that ID found so no reactions can be deleted from it." });
+      return;
+    }
+
+    let is_found = thoughtCheck.reactions.find(reaction => reaction.reactionId.toString() === reaction_id);
+
+    if(is_found === undefined) {
+      res.status(400).json({ message: "No reaction assoicated with this thought found with that ID." });
+      return;
+    }
+
     const delReactionQuery = await Thought.findOneAndUpdate(
-      { _id: req.params.thoughtId },
-      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { _id: thought_id },
+      { $pull: { reactions: { reactionId: reaction_id } } },
       { runValidators: true, new: true }
     );
 
